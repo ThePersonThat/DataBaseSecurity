8e464af129658206f21d8828718030fa954bd852
Oleksii Haidabrus <gaudabrus@gmail.com>
2021-05-06 00:49:46 +0300

    Added pagination database

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,12 +1,20 @@
-const pgClient = require('../models/pg-database')
+const pgClient = require('../models/pg-database');
 
 module.exports.getData = async (req, res) => {
-    const tournament = await pgClient.query('SELECT * FROM TOURNAMENT');
-    const race = await pgClient.query('SELECT * FROM RACE');
+    const limit = req.query.pagesize;
+    const offset = req.query.offset;
+
+    const tournament = await pgClient.query('SELECT * FROM TOURNAMENT OFFSET ' + offset + ' LIMIT ' + limit);
+    const tournamentCount = await pgClient.query('SELECT COUNT(*) FROM TOURNAMENT');
+
+    const race = await pgClient.query('SELECT * FROM RACE OFFSET ' + offset + ' LIMIT ' + limit);
+    const raceCount = await pgClient.query('SELECT COUNT(*) FROM RACE');
 
     const response = {
         tournaments: tournament.rows,
-        races: race.rows
+        tournamentCount: tournamentCount.rows[0].count,
+        races: race.rows,
+        raceCount: raceCount.rows[0].count
     };
 
     res.status(200).json(response);
