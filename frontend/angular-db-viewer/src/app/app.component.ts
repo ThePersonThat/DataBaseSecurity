04d73998d4d8db2180c5c5ea7ae37aeea88b8df5
Oleksii Haidabrus <gaudabrus@gmail.com>
2021-05-05 23:36:19 +0300

    Added material UI and the second task is completed

--- frontend/angular-db-viewer/src/app/app.component.ts
+++ frontend/angular-db-viewer/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {DataService} from "./shared/services/data.service";
 import {Race, Tournament} from "./shared/interfaces/Data";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-root',
@@ -10,17 +12,33 @@ import {Race, Tournament} from "./shared/interfaces/Data";
 })
 export class AppComponent {
   finish = false;
-  races: Race[];
-  tournaments: Tournament[];
 
-  constructor(private data: DataService) {
-  }
+  races: MatTableDataSource<Race>;
+  raceCount: number;
+  raceColumns: string[] = ['id_race', 'id_tournament', 'stage_of_tournament', 'max_rank'];
+  @ViewChild(MatPaginator) racePaginator: MatPaginator;
+
+  tournaments: MatTableDataSource<Tournament>;
+  tournamentCount: number;
+  tournamentColumns: string[] = ['id_tournament', 'location', 'count_stages', 'name'];
+  @ViewChild(MatPaginator) tournamentPaginator: MatPaginator;
+
+  COUNT_PER_PAGE = 5;
+
+  constructor(private data: DataService) {}
 
   getDataFromBackend(): void {
     this.data.getDataFromTwoTables().subscribe(
       value => {
-        this.races = value.races;
-        this.tournaments = value.tournaments;
+        this.races = new MatTableDataSource<Race>(value.races)
+        this.races.paginator = this.racePaginator;
+        this.raceCount = value.races.length;
+
+
+        this.tournaments = new MatTableDataSource<Tournament>(value.tournaments);
+        this.tournaments.paginator = this.tournamentPaginator;
+        this.tournamentCount = value.tournaments.length;
+
         this.finish = true;
       }
     )
