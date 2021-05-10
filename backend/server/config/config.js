79cfba2fd462979d1ef98953bbf1bf77676ca3d3
Oleksii Haidabrus <gaudabrus@gmail.com>
2021-05-11 02:32:24 +0300

    Added docker

--- backend/server/config/config.js
+++ backend/server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    postgresURI: 'postgresql://alex:root@localhost:5432/test',
+    postgresURI: 'postgresql://alex:root@postgress:5432/test',
     jwtKey: 'I NEED MORE POWER',
     jwtExpires: 60 * 60
 }
